4c59b965afe980a8e29ba30f4253b23e06a37882
2018180011woochan <wc5427@naver.com>
2021-05-22 20:06:17 +0900

    Search 수정

--- mySearch.py
+++ mySearch.py
@@ -1,18 +1,15 @@
 from tkinter import *
 from tkinter import font
 
-g_daywindow = Tk()
-g_daywindow.title("Weather_Reminder")
-g_daywindow.geometry("500x600")
-
-class MainGUI:
-    def InitSearchEntry(self):
+class mySearch:
+    global g_daywindow
+    def InitSearchEntry(self, g_daywindow):
         myFont =font.Font(g_daywindow, size=15, weight='bold')
         InputLabel =Entry(g_daywindow, font=myFont, width=15)
         InputLabel.pack()
         InputLabel.place(x=30, y=80)
 
-    def InitSearchButton(self):
+    def InitSearchButton(self, g_daywindow):
         myFont =font.Font(g_daywindow, size=10, weight='bold')
         SearchButton = Button(g_daywindow, font=myFont, text="검색",
                               command=self.SearchButtonAction)
@@ -25,6 +22,3 @@ class MainGUI:
     def __init__(self):
         self.InitSearchEntry()
         self.InitSearchButton()
-
-        g_daywindow.mainloop()
-MainGUI()
